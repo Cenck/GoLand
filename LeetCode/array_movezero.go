e9a5b21fe9356316693cfd3fc78bd9f0ca0e88bb
zhz <zhaohuzhi@souche.com>
2021-02-11 16:06:11 +0800

    hello go

---
+++ LeetCode/array_movezero.go
@@ -0,0 +1,17 @@
+package LeetCode
+
+
+func main() {
+	var arr = [0,1,0,3,12];
+	moveZeroes([0,1,0,3,12])
+}
+
+
+func moveZeroes(nums []int)  {
+
+	for i := 0; i < len(nums); i++ {
+		print(nums[i])
+	}
+
+}
+
